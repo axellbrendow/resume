f3ae90a7ab12559d8b324e1625f9d3dc618e7ea3
axell-brendow <breno.axel@gmail.com>
2023-05-11 11:03:09 -0300

    Updates

---
+++ precache-manifest.6bc5a56b0317a89ec5f11f70f692788d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca210af0338de603e68302493537a968",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "5e9b3c94072cbc8b828b",
+    "url": "/resume/static/css/main.72b5d9cb.chunk.css"
+  },
+  {
+    "revision": "0297c4084422f0065ca2",
+    "url": "/resume/static/js/2.a3423bac.chunk.js"
+  },
+  {
+    "revision": "39c0a85e5bfd9d1451e88dbbb3e8c862",
+    "url": "/resume/static/js/2.a3423bac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b418964566964b816b89",
+    "url": "/resume/static/js/3.c1b221a1.chunk.js"
+  },
+  {
+    "revision": "568d1296c4d2e326b8a8",
+    "url": "/resume/static/js/4.c74e1b32.chunk.js"
+  },
+  {
+    "revision": "5e9b3c94072cbc8b828b",
+    "url": "/resume/static/js/main.a318c8ef.chunk.js"
+  },
+  {
+    "revision": "a224939d17fc47d0e7c2",
+    "url": "/resume/static/js/runtime-main.51967445.js"
+  }
+]);
